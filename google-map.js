9aefe2b91ac6327696afe4d161691d4da6c563fd
yale <newmany89@gmail.com>
2018-02-14 10:03:59 +1100

    first commit

---
+++ google-map.js
@@ -0,0 +1,47 @@
+const data = {
+	API_KEY:'AIzaSyBiIzpZm4vKrXS_XCBPDOa6HL_4cFq1RWU'
+}
+
+const REC_SETTINGS = {
+		ll:'',
+		radius:500,
+		section:'drinks',
+		query:'cocktails',
+		venuePhotos:1,
+		openNow:1,
+		price:'2,3',
+		client_id:'MDDIFKZ5GFSAGZHAOFYPNQRATOT13FY2OYFUY1JDF5UNUZBA',
+		client_secret:'2ZUIA2A15LIKRSBGM3EN5BCCOX0YICNBJETKSRKOHDCQFSTT',
+		v:'20180210'
+	}
+
+function filterResults(response){
+	console.log(response);
+}
+
+function getRecs(response){
+	REC_SETTINGS.ll = [response.location.lat,response.location.lng].join()
+	console.log(REC_SETTINGS.ll);
+	const payload = {
+		url:'https://api.foursquare.com/v2/venues/explore',
+		dataType:'json',
+		data:REC_SETTINGS,
+		success: filterResults
+	}
+	$.ajax()
+}
+
+function getLocation(){
+	const payload = {
+		url:'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBiIzpZm4vKrXS_XCBPDOa6HL_4cFq1RWU',
+		method:'POST',
+		dataType:'json',
+		success:getRecs
+	}
+	$.post(payload)
+}
+
+
+$('#start').click(function(){
+	getLocation();
+});
