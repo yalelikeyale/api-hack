a111c2fc9d49bc972d13b05700185ffd04f4cc3f
yale <newmany89@gmail.com>
2018-02-17 15:28:56 +1100

    finished filtering recs now on to details

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const data = {
 	API_KEY:'AIzaSyBiIzpZm4vKrXS_XCBPDOa6HL_4cFq1RWU',
+	CLIENT_ID:'MDDIFKZ5GFSAGZHAOFYPNQRATOT13FY2OYFUY1JDF5UNUZBA',
+	CLIENT_SECRET:'2ZUIA2A15LIKRSBGM3EN5BCCOX0YICNBJETKSRKOHDCQFSTT',
+	VERSION:'20180210',
 	prefOrder:[],
 	priceArray:[],
-	minRating:''
+	minRating:'',
+	bonusOne:{
+		1:12,
+		2:10,
+		3:7,
+		4:3
+	},
+	bonusTwo:{
+		1:5,
+		2:4,
+		3:3,
+		4:2
+	}
+}
+
+const DET_SETTINGS = {
+	client_id:data.CLIENT_ID,
+	client_secret:data.CLIENT_SECRET,
+	v:data.VERSION
 }
 
 const REC_SETTINGS = {
@@ -14,27 +35,129 @@ const REC_SETTINGS = {
 		openNow:0,
 		time:'any',
 		price:'',
-		client_id:'MDDIFKZ5GFSAGZHAOFYPNQRATOT13FY2OYFUY1JDF5UNUZBA',
-		client_secret:'2ZUIA2A15LIKRSBGM3EN5BCCOX0YICNBJETKSRKOHDCQFSTT',
-		v:'20180210'
+		client_id:data.CLIENT_ID,
+		client_secret:data.CLIENT_SECRET,
+		v:data.VERSION
 	}
 
-function filterResults(response){
+function genVenCard(response){
+	console.log(response)
+}
+
+function getTopVenDetails(topVen){
+	let VEN_ID = Object.keys(topVen)[0];
+	console.log(topVen);
+	console.log(VEN_ID);
+	let payload = {
+		url:`https://api.foursquare.com/v2/venues/${VEN_ID}`,
+		dataType:'json',
+		data:DET_SETTINGS,
+		success: genVenCard
+	}
+	$.ajax(payload);
+}
+
+function getTopVenue(venues){
+	let topScore = 0
+	let topVenues = []
+	for (venue in venues){
+		let _id = Object.keys(venues[venue])[0]
+		let _score = venues[venue][_id].score
+		if (topScore < _score ) {
+			topScore = _score
+		}
+	}
+	for (venue in venues){
+		let _id = Object.keys(venues[venue])[0]
+		let _score = venues[venue][_id].score
+		if ( topScore === _score ) {
+			topVenues.push(venues[venue])
+		}
+	}
+	
+	for (venue in topVenues){
+		let _id = Object.keys(venues[venue])[0]
+		let _score = venues[venue][_id].score
+	}
+	getTopVenDetails(topVenues[0])
+}
+
+function scoreRecs(venues){
+	let totalVen = venues.length
+	for (venue in venues){
+		let score = 0;
+		let _id = Object.keys(venues[venue])[0]
+		let keys = Object.keys(venues[venue][_id])
+		for (key in keys){
+			let _rank = venues[venue][_id][keys[key]]
+			let _points = 0;
+			if (keys[key]==='priceTier'){
+				_points += (5 - _rank)
+				if ( (data.prefOrder.indexOf('priceTier')===0) && (_rank===1) ){
+					_points += 20
+				}
+			} else {
+				_points += (totalVen - _rank)
+				if ( (data.prefOrder.indexOf(keys[key])===0) && (_rank < 5) ){
+					_points += data.bonusOne[_rank.toString()]
+				}
+			}
+			if ( (data.prefOrder.indexOf(keys[key])===1) && (_rank < 5) ){
+				_points += data.bonusTwo[_rank.toString()]
+			}
+			venues[venue][_id][keys[key]] = _points;
+			score += _points;
+		}
+		venues[venue][_id].score = score;
+	}
+	getTopVenue(venues)
+}
+
+function genRecsObj(venue) {
+
 	let venues = {};
-	let recVenues = response.response.groups[0].items;
-	for(venue in recVenues){
-		console.log(recVenues[venue]);
-		venue = recVenues[venue];
-		let ven_ID = venue.venue.id;
-		venues[ven_ID] = {};
-		venues[ven_ID].rating = venue.venue.rating;
-		venues[ven_ID].priceTier = venue.venue.price.tier;
-		venues[ven_ID].distance = venue.venue.location.distance;
-		// if( rating > data.minRating ) {
+	let ven_ID = venue.venue.id;
+	venues[ven_ID] = {};
+	venues[ven_ID].rating = venue.venue.rating;
+	venues[ven_ID].priceTier = venue.venue.price.tier;
+	venues[ven_ID].distance = venue.venue.location.distance;
+	venues[ven_ID].checkins = venue.venue.stats.checkinsCount;
+	return venues;
+}
 
-		// }
+function rankResults(response){
+	let recVenues = response.response.groups[0].items;
+	let venues = recVenues.map( venue => genRecsObj(venue));
+	for (venue in venues) {
+		let _id = Object.keys(venues[venue])[0]
+		let _rating = venues[venue][_id].rating
+		let _dist = venues[venue][_id].distance
+		let _checkins = venues[venue][_id].checkins
+		let ratRank = 1
+		let distRank = 1
+		let popRank = 1
+		for (nextVen in venues){
+			let nextID = Object.keys(venues[nextVen])[0]
+			let nextRating = venues[nextVen][nextID].rating
+			let nextDistance = venues[nextVen][nextID].distance
+			let nextCheck = venues[nextVen][nextID].checkins
+			if( (venue !== nextVen) ){
+				if (nextRating > _rating){
+					ratRank++
+				}
+				if (nextDistance < _dist){
+					distRank++
+				}
+				if (nextCheck < _checkins){
+					popRank++
+				}
+			}
+		}
+		venues[venue][_id].rating = ratRank
+		venues[venue][_id].distance = distRank
+		venues[venue][_id].checkins = popRank
 	}
-	console.log(venues);
+	scoreRecs(venues)
 }
 
 function getRecs(response){
@@ -42,7 +165,7 @@ function getRecs(response){
 		url:'https://api.foursquare.com/v2/venues/explore',
 		dataType:'json',
 		data:REC_SETTINGS,
-		success: filterResults
+		success: rankResults
 	}
 	$.ajax(payload)
 }
@@ -55,7 +178,6 @@ function renderPreferences(response){
 		$('.card[role="button"]').removeClass('selected');
 		$(this).addClass('selected');
 		REC_SETTINGS.query = $(this).attr('data-query')
-		console.log(REC_SETTINGS.query);
 	});
 	let sliderValue = $('#rangeValue');
 	let sliderObject = $('#rangeFilter');
