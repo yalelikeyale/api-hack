28738a5912d5e704765e176d631149663eeab54b
yale <newmany89@gmail.com>
2018-03-03 13:48:11 +1100

    checkpoint

--- preferences/js/recEngine.js
+++ preferences/js/recEngine.js
@@ -62,6 +62,14 @@ function genVenCard(){
 	$('.js-ven-add').text(data.selectedVen.formatted_address);
 }
 
+function endLoading(){
+	$('.fa-check-circle').addClass('found');
+	const fadeEnd = $('#loading-page').fadeOut(2000,()=>{
+		genVenCard()
+	});
+	setTimeout(fadeEnd,1000)
+}
+
 function filterGoogleReviews(response){
 	let reviews = response.result.reviews;
 	reviews.forEach(review => {
@@ -73,6 +81,7 @@ function filterGoogleReviews(response){
 	});
 	if (data.searchAgain===false){
 		data.selectedVen = response.result;
+		endLoading()
 	} else {
 		if ((data.nextVen + 1 < data.venues.length)){
 			prepareSearch()
@@ -211,20 +220,70 @@ function rankResults(response){
 	scoreRecs()
 }
 
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomColor(type) {
+	  let c;
+	  if(type == "bright") {
+	    c = randomNumber(130, 255);
+	  } else {
+	    c = randomNumber(110, 190);
+	  }
+	  return "rgb(" + c + "," + c + "," + c + ")";
+	}
+
+function startDisco(){
+	let t = 1;
+	let radius = 50;
+	let squareSize = 6.5;
+	let prec = 19.55;
+	let fuzzy = 0.001;
+	let inc = (Math.PI-fuzzy)/prec;
+	let discoBall = document.getElementById("discoBall");
+
+	for(let t=fuzzy; t<Math.PI; t+=inc) {
+	  	let z = radius * Math.cos(t);
+	  	let currentRadius = Math.abs((radius * Math.cos(0) * Math.sin(t)) - (radius * Math.cos(Math.PI) * Math.sin(t))) / 2.5;
+	  	let circumference = Math.abs(2 * Math.PI * currentRadius);
+	  	let squaresThatFit = Math.floor(circumference / squareSize);
+	  	let angleInc = (Math.PI*2-fuzzy) / squaresThatFit;
+	  	for(let i=angleInc/2+fuzzy; i<(Math.PI*2); i+=angleInc) {
+	    	let square = document.createElement("div");
+	    	let squareTile = document.createElement("div");
+	    	squareTile.style.width = squareSize + "px";
+	    	squareTile.style.height = squareSize + "px";
+	    	squareTile.style.transformOrigin = "0 0 0";
+	    	squareTile.style.webkitTransformOrigin = "0 0 0";
+	    	squareTile.style.webkitTransform = "rotate(" + i + "rad) rotateY(" + t + "rad)";
+	    	squareTile.style.transform = "rotate(" + i + "rad) rotateY(" + t + "rad)";
+	    	if((t>1.3 && t<1.9) || (t<-1.3 && t>-1.9)) {
+	     		 squareTile.style.backgroundColor = randomColor("bright");
+	    	} else {
+	      		squareTile.style.backgroundColor = randomColor("any");
+	    	}
+	    	square.appendChild(squareTile);
+	    	square.className = "square";
+	    	squareTile.style.webkitAnimation = "reflect 2s linear infinite";
+	    	squareTile.style.webkitAnimationDelay = String(randomNumber(0,20)/10) + "s";
+	    	squareTile.style.animation = "reflect 2s linear infinite";
+	    	squareTile.style.animationDelay = String(randomNumber(0,20)/10) + "s";
+	    	squareTile.style.backfaceVisibility = "hidden";
+	    	let x = radius * Math.cos(i) * Math.sin(t);
+	    	let y = radius * Math.sin(i) * Math.sin(t);
+	    	square.style.webkitTransform = "translateX(" + Math.ceil(x) + "px) translateY(" + y + "px) translateZ(" + z + "px)";
+	    	square.style.transform = "translateX(" + x + "px) translateY(" + y + "px) translateZ(" + z + "px)";
+	    	discoBall.appendChild(square);
+	  	}
+	}
+}
+
 function loading(){
 	$('#fullpage').attr('hidden',true);
+	$('#nav-menu').attr('hidden',true);
 	$('#loading-page').attr('hidden',false);
-	const responses = ['Searching Venues in your area','comparing venues','we have found your venue'];
-	let idx = 1
-	$('.js-status-update').text(responses[0]);
-	const respInt = setInterval(()=>{
-		if(idx>responses.length && !data.searchAgain){
-			clearInterval(respInt);
-			genVenCard()
-		}
-		$('.js-status-update').text(responses[idx])
-		idx++
-	}, 3000)
+	startDisco()
 }
 
 function getRecs(response){
